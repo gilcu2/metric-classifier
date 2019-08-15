bbe3d50ba254c0ed1c946db6a3ccef646530ddef
Reynaldo Gil <gilcu2@gmail.com>
2019-08-15 12:24:48 +0100

    beginning changinf directions

--- src/main/scala/gilcu2/optimizations/OptimizerFunctionTwoPointsWithConstraints.scala
+++ src/main/scala/gilcu2/optimizations/OptimizerFunctionTwoPointsWithConstraints.scala
@@ -2,14 +2,14 @@ package gilcu2.optimizations
 
 import gilcu2.spaces.RNDensePoint
 
-case class Result(x1: RNDensePoint, x2: RNDensePoint, fValue: Double)
+case class Result(x1: RNDensePoint, x2: RNDensePoint, fValue: Double, lastDelta: Double)
 
 class Constraint(val f: RNDensePoint => Double,
                  val jacobian: RNDensePoint => RNDensePoint,
-                 val lowLimit: Double, val highLimit: Double) {
+                 val limit: Double) {
   def satisfy(x: RNDensePoint): Boolean = {
     val value = f(x)
-    value >= lowLimit && value <= highLimit
+    value <= limit
   }
 }
 
@@ -20,43 +20,128 @@ object OptimizerFunctionTwoPointsWithConstraints {
                gradFX2: (RNDensePoint, RNDensePoint) => RNDensePoint,
                constraintsX1: Vector[Constraint],
                constraintsX2: Vector[Constraint]
-              )(initialX1: RNDensePoint, initialX2: RNDensePoint, initialDelta: Double)(implicit epsilon: Double): Result = {
+              )(initialX1: RNDensePoint, initialX2: RNDensePoint, initialDelta: Double)(
+                implicit epsilon: Double): Result = {
+    var best = Result(initialX1, initialX2, f(initialX1, initialX2), initialDelta)
+    var cond = true
+    var rPrevious = optimizeOneDirection(f, gradFX1, gradFX2, constraintsX1, constraintsX2)(
+      initialX1, initialX2, initialDelta)
+    var rNew = rPrevious.copy()
+
+    while (cond) {
+      val newX1 = getNewXOtherDirection(constraintsX1, rPrevious.lastDelta, rPrevious.x1)
+      rNew = if ((newX1 - rPrevious.x1).norm > epsilon)
+        optimizeOneDirection(f, gradFX1, gradFX2, constraintsX1, constraintsX2)(
+          newX1, initialX2, initialDelta)
+      else rNew
+
+      val newX2 = getNewXOtherDirection(constraintsX2, rNew.lastDelta, rNew.x2)
+      rNew = if ((newX1 - rPrevious.x1).norm > epsilon)
+        optimizeOneDirection(f, gradFX1, gradFX2, constraintsX1, constraintsX2)(
+          newX1, initialX2, initialDelta)
+      else rNew
+
+      if (rNew.fValue < rPrevious.fValue) {
+        rPrevious = rNew
+        rNew = rPrevious.copy()
+      }
+      else
+        cond = false
+    }
+    rPrevious
+  }
+
+  def optimizeOneDirection(f: (RNDensePoint, RNDensePoint) => Double,
+                           gradFX1: (RNDensePoint, RNDensePoint) => RNDensePoint,
+                           gradFX2: (RNDensePoint, RNDensePoint) => RNDensePoint,
+                           constraintsX1: Vector[Constraint],
+                           constraintsX2: Vector[Constraint])(
+                            initialX1: RNDensePoint, initialX2: RNDensePoint,
+                            initialDelta: Double
+                          )(implicit epsilon: Double): Result = {
     var delta = initialDelta
     var prevX1 = initialX1
     var prevX2 = initialX2
     var prevF = f(prevX1, prevX2)
     var cond = true
+
     while (cond) {
-      val gradFX1Value = gradFX1(prevX1, prevX2)
+      val (newX1, newX2) =
+        (
+          getNewX1InLine(gradFX1, constraintsX1, delta, prevX1, prevX2),
+          getNewX2InLine(gradFX2, constraintsX2, delta, prevX1, prevX2)
+        )
 
-      val newX1Try = prevX1 - gradFX1Value * delta
-      val newX1 = if (constraintsX1.forall(_.satisfy(newX1Try)))
-        newX1Try
-      else
-        prevX1
-
-      val gradFX2Value = gradFX2(prevX1, prevX2)
-      val newX2Try = prevX2 - gradFX2Value * delta
-      val newX2 = if (constraintsX2.forall(_.satisfy(newX2Try)))
-        newX2Try
-      else
-        prevX2
-
-      if ((newX1 - prevX1).norm > 0.0 || (newX2 - prevX2).norm > 0.0) {
+      if (areNewPointsDifferent(prevX1, prevX2, newX1, newX2)) {
         val newF = f(newX1, newX2)
         if (prevF > newF) {
           prevX1 = newX1
           prevX2 = newX2
           prevF = newF
-        } else
+        } else {
           cond = false
+        }
       }
       else if (delta > epsilon)
         delta /= 2
       else
         cond = false
+
     }
-    Result(prevX1, prevX2, prevF)
+    Result(prevX1, prevX2, prevF, delta)
+  }
+
+  private def areNewPointsDifferent(prevX1: RNDensePoint, prevX2: RNDensePoint,
+                                    newX1: RNDensePoint, newX2: RNDensePoint): Boolean
+
+  =
+    (newX1 - prevX1).norm > 0.0 || (newX2 - prevX2).norm > 0.0
+
+  private def getNewX2InLine(gradFX2: (RNDensePoint, RNDensePoint) => RNDensePoint,
+                             constraintsX2: Vector[Constraint], delta: Double,
+                             prevX1: RNDensePoint, prevX2: RNDensePoint): RNDensePoint
+
+  = {
+    val gradFX2Value = gradFX2(prevX1, prevX2)
+    val newX2Try = prevX2 - gradFX2Value * delta
+    val newX2 = if (constraintsX2.forall(_.satisfy(newX2Try)))
+      newX2Try
+    else
+      prevX2
+    newX2
+  }
+
+  private def getNewX1InLine(gradFX1: (RNDensePoint, RNDensePoint) => RNDensePoint,
+                             constraintsX1: Vector[Constraint],
+                             delta: Double, prevX1: RNDensePoint, prevX2: RNDensePoint): RNDensePoint
+
+  = {
+    val gradFX1Value = gradFX1(prevX1, prevX2)
+
+    val newX1Try = prevX1 - gradFX1Value * delta
+    val newX1 = if (constraintsX1.forall(_.satisfy(newX1Try)))
+      newX1Try
+    else
+      prevX1
+    newX1
+  }
+
+  private def getNewXOtherDirection(constraintsX1: Vector[Constraint],
+                                    delta: Double, prevX: RNDensePoint)(
+                                     implicit epsilon: Double
+                                   ): RNDensePoint
+
+  = {
+
+    val constraints_equal = constraintsX1.filter(constraint => (constraint.f(prevX) - constraint.limit) < epsilon)
+
+    if (constraints_equal.length == 1) {
+      val newX = prevX + constraints_equal.head.jacobian(prevX) * delta
+      newX
+    }
+    else
+      prevX
+
   }
 
 }
--- src/test/unit/scala/gilcu2/optimizations/OptimizerFunctionTwoPointsWithConstraintsTest.scala
+++ src/test/unit/scala/gilcu2/optimizations/OptimizerFunctionTwoPointsWithConstraintsTest.scala
@@ -35,7 +35,7 @@ class OptimizerFunctionTwoPointsWithConstraintsTest extends FlatSpec with Matche
     def jacobianConstraintBall1(x: RNDensePoint): RNDensePoint =
       (x - ball1.center)
 
-    val constraintBall1 = new Constraint(fConstraintBall1, jacobianConstraintBall1, 0, ball1.radio)
+    val constraintBall1 = new Constraint(fConstraintBall1, jacobianConstraintBall1, ball1.radio)
 
     def fConstraintBall2(x: RNDensePoint): Double =
       (x - ball2.center).norm
@@ -43,9 +43,54 @@ class OptimizerFunctionTwoPointsWithConstraintsTest extends FlatSpec with Matche
     def jacobianConstraintBall2(x: RNDensePoint): RNDensePoint =
       (x - ball2.center)
 
-    val constraintBall2 = new Constraint(fConstraintBall2, jacobianConstraintBall2, 0, ball2.radio)
+    val constraintBall2 = new Constraint(fConstraintBall2, jacobianConstraintBall2, ball2.radio)
 
     val (initialX1, initialX2) = (ball1.center, ball2.center)
+    val initialDelta = math.max(ball1.radio, ball2.radio)
+
+    When("the function is minimized")
+    val r = minimize(distance, gradDistanceX1, gradDistanceX2,
+      Vector(constraintBall1), Vector(constraintBall2))(initialX1, initialX2, initialDelta)
+
+    Then("results must be the expected")
+    (r.x1 - RNDensePoint(1.0, 0.0)).norm should be < epsilon
+    (r.x2 - RNDensePoint(2.0, 0.0)).norm should be < epsilon
+    r.fValue shouldBe 1.0 +- epsilon
+  }
+
+  it should "find the minimum distance between to balls, initials not in line with solution" in {
+
+    Given("the function,restriction, jacobians and balls")
+
+    val ball1 = Ball(RNDensePoint(0.0, 0.0), 1.0)
+    val ball2 = Ball(RNDensePoint(3.0, 0.0), 1.0)
+
+    def distance(x1: RNDensePoint, x2: RNDensePoint): Double =
+      (x1 - x2).norm
+
+    def gradDistanceX1(x1: RNDensePoint, x2: RNDensePoint): RNDensePoint =
+      x1 - x2
+
+    def gradDistanceX2(x1: RNDensePoint, x2: RNDensePoint): RNDensePoint =
+      x2 - x1
+
+    def fConstraintBall1(x: RNDensePoint): Double =
+      (x - ball1.center).norm
+
+    def jacobianConstraintBall1(x: RNDensePoint): RNDensePoint =
+      (x - ball1.center)
+
+    val constraintBall1 = new Constraint(fConstraintBall1, jacobianConstraintBall1, ball1.radio)
+
+    def fConstraintBall2(x: RNDensePoint): Double =
+      (x - ball2.center).norm
+
+    def jacobianConstraintBall2(x: RNDensePoint): RNDensePoint =
+      (x - ball2.center)
+
+    val constraintBall2 = new Constraint(fConstraintBall2, jacobianConstraintBall2, ball2.radio)
+
+    val (initialX1, initialX2) = (ball1.center - RNDensePoint(0, 0.5), ball2.center - RNDensePoint(0, 0.5))
     val initialDelta = math.max(ball1.radio, ball2.radio)
 
     When("the function is minimized")
